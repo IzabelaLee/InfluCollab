68ed79c9692c03904f64afacc90cce8a61af34ef
Izabela <izabela.leng@gmail.com>
2026-06-14 23:32:53 +0200

    Spring Boot initial setup

---
+++ src/main/java/com/influcollab/InfluCollabApplication.java
@@ -0,0 +1,13 @@
+package com.influcollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluCollabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfluCollabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/influcollab/InfluCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.influcollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluCollabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
