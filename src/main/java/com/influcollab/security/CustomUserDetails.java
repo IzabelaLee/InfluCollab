552aac48ed2c31a1e7bb34e9dffcc31c5db29953
Izabela <izabela.leng@gmail.com>
2026-07-29 00:47:50 +0200

    Add Spring Security authentication infrastructure

---
+++ src/main/java/com/influcollab/security/CustomUserDetails.java
@@ -0,0 +1,49 @@
+package com.influcollab.security;
+
+import com.influcollab.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public java.util.Collection<? extends org.springframework.security.core.GrantedAuthority> getAuthorities() {
+        // Return the user's roles/authorities if applicable
+        return java.util.Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
